a66b359f990561fa99688f424738dba4088c7ebd
Kajanth Kumar <kajanthkumar@gmail.com>
2020-06-21 19:26:00 -0400

    code clean up and comments

--- js/script.js
+++ js/script.js
@@ -5,9 +5,10 @@ const wordShift = {};
 wordShift.init = () => {
     wordShift.countUserInput();
     // wordShift.countText('userOutput','afterWord', 'afterChar');
+    wordShift.optimize();
 }
 
-
+// function that counts the user text field
 wordShift.countUserInput = () => {
     document.getElementById('userInput').addEventListener('input', () => {
         wordShift.countText('userInput','beforeWord', 'beforeChar');
@@ -20,10 +21,7 @@ wordShift.countText = (textSource, wordOutput, charOutput) => {
     document.getElementById(charOutput).innerHTML=`Characters: ${wordShift.charCount(textSource)}`;
 }
 
-
-
-
-//function that counts words and characters accepts a string
+//function that counts and returns the number of words
 wordShift.wordCount = (textInput) => {
     const input = document.getElementById(textInput).value;
     // To count words, trim the ends of the input,
@@ -36,11 +34,19 @@ wordShift.wordCount = (textInput) => {
     return wordCount
 }
 
+// function that counts and returns the number of characters
 wordShift.charCount = (textInput) => {
     let input = document.getElementById(textInput).value;
     // to count characters, split the string by whitespacees and return length of joned substring
     const charCount = input.split(" ").join("").length;
     return charCount;
+}
+
+// function that takes a string and optimizes it for less characters
+wordShift.optimize = () => {
+    document.getElementById('optimize').addEventListener('click', () => {
+        console.log("clicked");
+    });
 }
 
 // document ready
